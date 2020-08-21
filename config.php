ec2564d7ede4dd3ad11bf0fac117f9c0271567a2
DellCrow <52834918+DellCrow@users.noreply.github.com>
2020-08-21 20:23:40 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+	session_start();
+	define("HOME_URI", "https://zillion-males.000webhostapp.com/");
+	define("HOME_DIR", "./");
+	/*CONSTANTES DE CONEXAO AO BANCO DE DADOS */
+	define('SGBD','mysql');
+	define('HOST_DB','000webhost.com');
+	define('DB','id9735697_news');
+	define('USER_DB','root');
+	define('PASS_DB','g&p4m]iopSUm+Q(W');
+?>
